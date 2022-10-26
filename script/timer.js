3a2f6a4283cc1732c80dc59619ff0757069b7dab
Hafizzankadir <60392004+Hafizzankadir@users.noreply.github.com>
2022-10-26 15:14:29 +0800

    update timer.js

--- script/timer.js
+++ script/timer.js
@@ -1,76 +1,119 @@
-// variables
+// * variable
 
-let workTitle = document.getElementById('work');
-let breakTitle = document.getElementById('break');
+let start = document.getElementById("start");
+let stop = document.getElementById("pause");
+let reset = document.getElementById("reset");
 
+let workMin = document.getElementById("work_minutes");
+let workSec = document.getElementById("work_seconds");
 
-// set the duration in minutes
+let breakMin = document.getElementById("break_minutes");
+let breakSec = document.getElementById("break_seconds");
 
-// this minutes is base on the pomodoro principles
-let workTime = 25;
-let breakTime = 5;
+const workValue = localStorage.getItem("workTime");
+const breakValue = localStorage.getItem("breakTime");
 
-let seconds = "00"
+let workTitle = document.getElementById("work");
+let breakTitle = document.getElementById("break");
 
-// display time
+let workTime = workValue;
+let breakTime = breakValue;
+
+let seconds = "00";
+
+// * set the timer based on timer input in form page
 
 window.onload = () => {
-    document.getElementById('minutes').innerHTML = workTime;
-    document.getElementById('seconds').innerHTML = seconds;
+  workMin.innerText = workTime;
+  workSec.innerText = seconds;
+  breakMin.innerText = breakTime;
+  breakSec.innerText = seconds;
 
-    workTitle.classList.add('active');
+  workTitle.classList.add("active");
+};
+
+// * start timer
+
+let startTimer;
+
+start.addEventListener("click", function () {
+  if (startTimer === undefined) {
+    startTimer = setInterval(timer, 1000);
+    document.getElementById("start").style.display = "none";
+    document.getElementById("pause").style.display = "inline";
+  } else {
+    alert("Timer is already running");
+  }
+});
+
+// * reset timer
+
+reset.addEventListener("click", function () {
+  workMin.innerText = workTime;
+  workSec.innerText = "00";
+
+  breakMin.innerText = breakTime;
+  breakSec.innerText = "00";
+
+  document.getElementById("start").style.display = "inline";
+  document.getElementById("pause").style.display = "none";
+
+  stopInterval();
+  startTimer = undefined;
+});
+
+// * pause the timer
+
+stop.addEventListener("click", function () {
+  stopInterval();
+  startTimer = undefined;
+  document.getElementById("start").style.display = "inline";
+  document.getElementById("pause").style.display = "none";
+});
+
+// * timer function
+
+function timer() {
+  // work timer countdown
+
+  if (workSec.innerText != 0) {
+    workSec.innerText--;
+  } else if (workMin.innerText != 0 && workSec.innerText == 0) {
+    workSec.innerText = 59;
+    workMin.innerText--;
+  }
+
+  // break timer countdown
+  if (workMin.innerText == 0 && workSec.innerText == 0) {
+    if (breakSec.innerText != 0) {
+      breakSec.innerText--;
+      workTitle.classList.remove("active");
+      breakTitle.classList.add("active");
+    } else if (breakMin.innerText != 0 && breakSec.innerText == 0) {
+      breakSec.innerText = 59;
+      breakMin.innerText--;
+    }
+  }
+
+  // full cycle
+
+  if (
+    workMin.innerText == 0 &&
+    workSec.innerText == 0 &&
+    breakMin.innerText == 0 &&
+    breakSec.innerText == 0
+  ) {
+    workTitle.classList.add("active");
+    breakTitle.classList.remove("active");
+    workMin.innerText = workTime;
+    workSec.innerText = "00";
+    breakMin.innerText = breakTime;
+    breakSec.innerText = "00";
+  }
 }
 
-// start timer
+// * stop timer function
 
-function start() {
-    // change button
-    document.getElementById('start').style.display = "none";
-    document.getElementById('reset').style.display = "block";
-
-    // change the time
-    seconds = 59;
-
-    let workMinutes = workTime - 1;
-    let breakMinutes = breakTime - 1;
-
-    breakCount = 0;
-
-    // countdown
-    let timerFunction = () => {
-        //change the display
-        document.getElementById('minutes').innerHTML = workMinutes;
-        document.getElementById('seconds').innerHTML = seconds;
-
-        // start
-        seconds = seconds - 1;
-
-        if(seconds === 0) {
-            workMinutes = workMinutes - 1;
-            if(workMinutes === -1 ){
-                if(breakCount % 2 === 0) {
-                    // start break
-                    workMinutes = breakMinutes;
-                    breakCount++
-
-                    // change the label
-                    workTitle.classList.remove('active');
-                    breakTitle.classList.add('active');
-                }else {
-                    // continue work
-                    workMinutes = workTime;
-                    breakCount++
-
-                    // change the label
-                    breakTitle.classList.remove('active');
-                    workTitle.classList.add('active');
-                }
-            }
-            seconds = 59;
-        }
-    }
-
-    // start countdown
-    // In this case I use 10 to fast forward the minutes to second
-    setInterval(timerFunction, 10); // 1000 = 1s 
+function stopInterval() {
+  clearInterval(startTimer);
 }
